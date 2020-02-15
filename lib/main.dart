28de731b797caaf6cc2db99afd8cff8024ac5fc1
kys6879 <kys6879@naver.com>
2020-02-15 22:13:35 +0900

    button layout 수정

--- lib/main.dart
+++ lib/main.dart
@@ -3,91 +3,75 @@ import 'package:english_words/english_words.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatefulWidget {
-  // 위젯 부분, 스테이트를 다루는 부분은 분리되어 있다.
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  // 스테이트를 관리하고, UI를 그리는 부분.
-
-  int count = 0;
-
-  void plusCount() {
-    setState(() {
-      count++;
-    });
-  }
-
-  void minusCount() {
-    setState(() {
-      count--;
-    });
-  }
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        title: "첫 Stateful App",
-        home: Scaffold(
-            appBar: AppBar(
-              title: Text("계산기!"),
-            ),
-            body: Container(
-              padding: const EdgeInsets.all(16.0),
+    Widget titleSection = Container(
+        padding: EdgeInsets.all(32),
+        child: Row(
+          children: <Widget>[
+            Expanded(
               child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: <Widget>[
-                  Text(
-                    "$count",
-                    style: TextStyle(fontSize: 80.0),
+                  Container(
+                    padding: EdgeInsets.only(bottom: 8),
+                    child: Text(
+                      "Oeschinen Lake Campground",
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
                   ),
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: <Widget>[
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: <Widget>[
-                          btnWidget("7"),
-                          btnWidget("8"),
-                          btnWidget("9"),
-                          btnWidget("x")
-                        ],
-                      ),
-                      SizedBox(height: 10),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: <Widget>[
-                          btnWidget("4"),
-                          btnWidget("5"),
-                          btnWidget("6"),
-                          btnWidget("-")
-                        ],
-                      ),
-                      SizedBox(height: 10),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: <Widget>[
-                          btnWidget("1"),
-                          btnWidget("2"),
-                          btnWidget("3"),
-                          btnWidget("+")
-                        ],
-                      )
-                    ],
+                  Text(
+                    'Kandersteg, Switzerland',
+                    style: TextStyle(color: Colors.grey[500]),
                   )
                 ],
               ),
-            )));
+            ),
+            Icon(
+              Icons.star,
+              color: Colors.red[500],
+            ),
+            Text("41")
+          ],
+        ));
+
+    Color color = Theme.of(context).primaryColor;
+
+    Widget buttonsection = Container(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          _buildButtonColumn(color, Icons.call, 'CALL'),
+          _buildButtonColumn(color, Icons.near_me, 'ROUTE'),
+          _buildButtonColumn(color, Icons.share, 'SHARE'),
+        ],
+      ),
+    );
+
+    return MaterialApp(
+      title: "fultter",
+      home: Scaffold(
+          appBar: AppBar(title: Text('layout')),
+          body: Column(
+            children: <Widget>[titleSection, buttonsection],
+          )),
+    );
   }
 
-  Widget btnWidget(var text) {
-    return ButtonTheme(
-      minWidth: 80.0,
-      height: 50.0,
-      child: RaisedButton(
-        child: Text("$text"),
-      ),
+  Column _buildButtonColumn(Color color, IconData icon, String label) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Icon(icon, color: color),
+        Container(
+          margin: const EdgeInsets.only(top: 8),
+          child: Text(label,
+              style: TextStyle(
+                  fontSize: 12, fontWeight: FontWeight.w400, color: color)),
+        )
+      ],
     );
   }
 }
