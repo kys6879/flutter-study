a7f4f0831ed34e727afb143cbee9ff1f07274997
kys6879 <kys6879@naver.com>
2020-02-18 21:11:49 +0900

    body low 완성

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/material.dart';
 import './GradientAppBar.dart';
+import 'package:my_app/HomePageBody.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         body: Column(
-      children: <Widget>[GradientAppBar("AppBar!")],
+      children: <Widget>[
+        GradientAppBar("AppBar!"),
+        HomePageBody(),
+        HomePageBody(),
+        HomePageBody()
+      ],
     ));
   }
 }
---
+++ lib/HomePageBody.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:my_app/PlanetRow.dart';
+
+class HomePageBody extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return PlanetRow();
+  }
+}
