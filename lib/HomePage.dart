c84e816f6663fa5affbe0bcac0a189cef14b36c2
kys6879 <kys6879@naver.com>
2020-02-20 21:59:55 +0900

    bottonbar 구성

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -1,18 +1,42 @@
 import 'package:flutter/material.dart';
-import './GradientAppBar.dart';
-import 'package:my_app/HomePageBody.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  HomePage();
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _selectedIndex = 0;
+
+  List<Widget> _widgetOptions = <Widget>[
+    Text('게시판 메뉴입니다.'),
+    Text('글쓰기 메뉴입니다.'),
+    Text('내정보 메뉴입니다.')
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Column(
-      children: <Widget>[
-        GradientAppBar("AppBar!"),
-        HomePageBody(),
-        HomePageBody(),
-        HomePageBody()
-      ],
-    ));
+      appBar: AppBar(title: Text("게시판")),
+      body: Center(child: _widgetOptions.elementAt(_selectedIndex)),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(icon: Icon(Icons.home), title: Text('홈')),
+          BottomNavigationBarItem(icon: Icon(Icons.edit), title: Text('글쓰기')),
+          BottomNavigationBarItem(icon: Icon(Icons.face), title: Text('내정보'))
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.amber[800],
+        onTap: _onItemTapped,
+      ),
+    );
   }
 }
--- lib/HomePageBody.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:my_app/PlanetRow.dart';
-
-class HomePageBody extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return PlanetRow();
-  }
-}
